5552ba09a4dfda219a23276d757b162d33c4df5f
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-08-10 22:41:51 +0530

    valid anagram

---
+++ day_5_valid_anagram.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] p= s.toCharArray();
+        char[] q= t.toCharArray();
+        Arrays.sort(p);
+        Arrays.sort(q);
+        return Arrays.equals(p,q);
+        
+    }
+}
